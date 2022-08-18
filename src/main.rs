b4d2ef82abed8bc0ac814c2dfc95e1aa5beb4186
Juraj Michálek <juraj.michalek@espressif.com>
2022-08-18 12:12:07 +0200

    fix RtcCntl dependency on hal

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ use hal::{
     prelude::*,
     spi,
     timer::TimerGroup,
-    RtcCntl,
+    Rtc,
     IO,
     Delay,
 };
@@ -63,13 +63,17 @@ fn main() -> ! {
     let mut clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
     // Disable the RTC and TIMG watchdog timers
-    let mut rtc_cntl = RtcCntl::new(peripherals.RTC_CNTL);
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
     let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
     let mut wdt0 = timer_group0.wdt;
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
 
-    rtc_cntl.set_wdt_global_enable(false);
+    #[cfg(feature="esp32c3")]
+    rtc.swd.disable();
+    #[cfg(feature="xtensa-lx-rt")]
+    rtc.rwdt.disable();
+
     wdt0.disable();
     wdt1.disable();
 
