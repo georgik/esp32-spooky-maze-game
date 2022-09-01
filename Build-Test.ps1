41866ebcf1ef276159fb1ff49af583ae8779457e
Juraj Michálek <juraj.michalek@espressif.com>
2022-09-01 09:36:35 +0200

    Feature/esp32 s3 box (#7)
    * add support for ESP32-S3-BOX

---
+++ Build-Test.ps1
@@ -0,0 +1,11 @@
+
+"cargo build --release --target xtensa-esp32-none-elf --features esp32_wrover_kit"
+cargo build --release --target xtensa-esp32-none-elf --features esp32_wrover_kit
+"cargo build --release --target xtensa-esp32s2-none-elf --features esp32s2_ili9341"
+cargo build --release --target xtensa-esp32s2-none-elf --features esp32s2_ili9341
+"cargo build --release --target xtensa-esp32s2-none-elf --features esp32s2_usb_otg"
+cargo build --release --target xtensa-esp32s2-none-elf --features esp32s2_usb_otg
+"cargo build --release --target xtensa-esp32s3-none-elf --features esp32s3_box"
+cargo build --release --target xtensa-esp32s3-none-elf --features esp32s3_box
+"cargo +nightly-2022-08-15 build --release --target riscv32imac-unknown-none-elf --features esp32c3_ili9341"
+cargo +nightly-2022-08-15 build --release --target riscv32imac-unknown-none-elf --features esp32c3_ili9341
