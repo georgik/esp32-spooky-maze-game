2bf1f32617855d8452ae979546523314181428d6
Juraj Michálek <juraj.michalek@gmail.com>
2022-12-19 06:44:00 +0100

    add web based accelerometer

--- wasm/index.js
+++ wasm/index.js
@@ -2,6 +2,8 @@ var keyUp;
 const rust = import('./pkg')
   .then(m => {
     let universe = m.Universe.new();
+    var motion_request = 0;
+
     document.getElementById('button-up').addEventListener('click', () => {
         universe.move_up();
     });
@@ -21,6 +23,22 @@ const rust = import('./pkg')
         universe.place_dynamite();
     });
 
+    window.addEventListener("devicemotion", (event) => {
+        if (event.accelerationIncludingGravity.y > 5) {
+            motion_request = 1;
+            console.log("up");
+        } else if (event.accelerationIncludingGravity.y < -5) {
+            motion_request = 2;
+            console.log("down");
+        } else if (event.accelerationIncludingGravity.x < -5) {
+            motion_request = 3;
+            console.log("left");
+        } else if (event.accelerationIncludingGravity.x > 5) {
+            motion_request = 4;
+            console.log("right");
+        }
+    }, true);
+
     document.addEventListener('keydown', (event) => {
         if ((event.key === "Up") || (event.key == "ArrowUp")) {
             universe.move_up();
@@ -37,6 +55,18 @@ const rust = import('./pkg')
     var oldTimestamp = 0;
     function renderFrame(timestamp) {
         if (timestamp - oldTimestamp > 200) {
+
+            if (motion_request == 1) {  // up
+                universe.move_up();
+            } else if (motion_request == 2) {  // down
+                universe.move_down();
+            } else if (motion_request == 3) {  // left
+                universe.move_left();
+            } else if (motion_request == 4) {  // right
+                universe.move_right();
+            }
+            motion_request = 0;
+
             universe.render_frame();
             oldTimestamp = timestamp;
         }
