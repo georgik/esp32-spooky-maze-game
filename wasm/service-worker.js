f92da5bc47d8265ebb9b2629aaf221b8c9bc170a
Juraj Michalek <juraj.michalek@espressif.com>
2022-12-19 10:21:01 +0100

    add pwa

---
+++ wasm/service-worker.js
@@ -0,0 +1,14 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst({
+        cacheName: 'images',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            }),
+        ],
+    }),
+);
