a6ab1d25305b0c2938e352ce13306b21a31a4925
Juraj Michálek <juraj.michalek@espressif.com>
2023-08-29 14:48:05 +0200

    accel controller working

--- esp32-s3-box/src/accel_movement_controller.rs
+++ esp32-s3-box/src/accel_movement_controller.rs
@@ -1,12 +1,6 @@
-// accel_movement_controller.rs
-
 use spooky_core::engine::Action;
 use spooky_core::movement_controller::MovementController;
 use icm42670::accelerometer::Accelerometer;
-// #[cfg(any(feature = "imu_controls"))]
-// use icm42670::{accelerometer::Accelerometer, Address, Icm42670};
-// #[cfg(any(feature = "imu_controls"))]
-// use shared_bus::BusManagerSimple;
 pub struct AccelMovementController<I>
 where
     I: Accelerometer,
@@ -56,6 +50,17 @@ where
     }
 
     fn tick(&mut self) {
+        if let Ok(accel_norm) = self.icm.accel_norm() {
+            if accel_norm.y > self.accel_threshold {
+                self.last_action = Action::Left;
+            } else if accel_norm.y < -self.accel_threshold {
+                self.last_action = Action::Right;
+            } else if accel_norm.x > self.accel_threshold {
+                self.last_action = Action::Down;
+            } else if accel_norm.x < -self.accel_threshold {
+                self.last_action = Action::Up;
+            }
+            // Additional actions for Teleport and PlaceDynamite can be added here
+        }
     }
-
 }
