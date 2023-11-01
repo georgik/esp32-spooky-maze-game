edaae0f478b4e4c2e79f09aac4d1e265cf86cc4d
Juraj Michálek <juraj.michalek@espressif.com>
2023-11-01 13:52:01 +0100

    esp32-c3-devkit-rust - fix implementation and update hal

--- esp32-c3-devkit-rust/src/accel_movement_controller.rs
+++ esp32-c3-devkit-rust/src/accel_movement_controller.rs
@@ -37,9 +37,9 @@ where
     fn tick(&mut self) {
         if let Ok(accel_norm) = self.icm.accel_norm() {
             if accel_norm.y > self.accel_threshold {
-                self.last_action = Action::Left;
-            } else if accel_norm.y < -self.accel_threshold {
                 self.last_action = Action::Right;
+            } else if accel_norm.y < -self.accel_threshold {
+                self.last_action = Action::Left;
             } else if accel_norm.x > self.accel_threshold {
                 self.last_action = Action::Down;
             } else if accel_norm.x < -self.accel_threshold {
--- esp32-c3-devkit-rust/src/setup.rs
+++ esp32-c3-devkit-rust/src/setup.rs
@@ -1,16 +1,16 @@
-use crate::types::{UnconfiguredPins, ConfiguredPins, ConfiguredSystemPins};
-use embedded_hal::digital::v2::{OutputPin, InputPin};
+use crate::types::{UnconfiguredPins, ConfiguredSystemPins};
+use embedded_hal::digital::v2::OutputPin;
 use hal::gpio::{self, Pins};
-use spooky_embedded::{ button_keyboard::ButtonKeyboard, embedded_movement_controller::EmbeddedMovementController };
-use spooky_core;
 
 pub fn setup_pins(pins: Pins) -> (UnconfiguredPins<gpio::Unknown>, /*ConfiguredPins<impl InputPin, impl InputPin, impl InputPin, impl InputPin, impl InputPin,
     impl InputPin>, */ConfiguredSystemPins<impl OutputPin, impl OutputPin, impl OutputPin>) {
             let unconfigured_pins = UnconfiguredPins {
-        sclk: pins.gpio6,
-        mosi: pins.gpio7,
-        sda: pins.gpio8,
-        scl: pins.gpio18,
+        sclk: pins.gpio0,
+        mosi: pins.gpio6,
+        miso: pins.gpio11,
+        cs: pins.gpio5,
+        sda: pins.gpio10,
+        scl: pins.gpio8,
     };
 
     // let configured_pins = ConfiguredPins {
@@ -23,39 +23,10 @@ pub fn setup_pins(pins: Pins) -> (UnconfiguredPins<gpio::Unknown>, /*ConfiguredP
     // };
 
     let configured_system_pins = ConfiguredSystemPins {
-        dc: pins.gpio21.into_push_pull_output(),
-        backlight: pins.gpio5.into_push_pull_output(),
+        dc: pins.gpio4.into_push_pull_output(),
+        backlight: pins.gpio1.into_push_pull_output(),
         reset: pins.gpio3.into_push_pull_output(),
     };
 
     (unconfigured_pins, /*configured_pins, */configured_system_pins)
-}
-
-pub fn setup_button_keyboard<Up: InputPin, Down: InputPin, Left: InputPin, Right: InputPin, Dyn: InputPin, Tel: InputPin>(
-    configured_pins: ConfiguredPins<Up, Down, Left, Right, Dyn, Tel>
-) -> ButtonKeyboard<Up, Down, Left, Right, Dyn, Tel> {
-    ButtonKeyboard::new(
-        configured_pins.up_button,
-        configured_pins.down_button,
-        configured_pins.left_button,
-        configured_pins.right_button,
-        configured_pins.dynamite_button,
-        configured_pins.teleport_button,
-    )
-}
-
-pub fn setup_movement_controller<Up, Down, Left, Right, Dyn, Tel>(
-    seed_buffer: [u8; 32],
-    button_keyboard: ButtonKeyboard<Up, Down, Left, Right, Dyn, Tel>
-) -> EmbeddedMovementController<Up, Down, Left, Right, Dyn, Tel>
-where
-    Up: InputPin,
-    Down: InputPin,
-    Left: InputPin,
-    Right: InputPin,
-    Dyn: InputPin,
-    Tel: InputPin,
-{
-    let demo_movement_controller = spooky_core::demo_movement_controller::DemoMovementController::new(seed_buffer);
-    EmbeddedMovementController::new(demo_movement_controller, button_keyboard)
 }
