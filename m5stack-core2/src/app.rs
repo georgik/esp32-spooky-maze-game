6181021472702408a6447d77a76f2146b5655c55
Juraj Michálek <juraj.michalek@espressif.com>
2023-11-10 13:37:46 +0100

    m5stack-core2 add DMA

--- m5stack-core2/src/app.rs
+++ m5stack-core2/src/app.rs
@@ -1,17 +1,24 @@
 use crate::{m5stack_composite_controller::M5StackCompositeController, accel_device::AccelDevice};
-use embedded_graphics::{pixelcolor::Rgb565, prelude::DrawTarget};
+use embedded_graphics::pixelcolor::Rgb565;
 use spooky_core::{engine::Engine, spritebuf::SpriteBuf, universe::Universe};
 use embedded_graphics_framebuf::FrameBuf;
 use embedded_graphics::prelude::RgbColor;
+use display_interface::WriteOnlyDataCommand;
+use mipidsi::models::Model;
+use embedded_hal::digital::v2::OutputPin;
 use crate::accel_movement_controller::AccelMovementController;
 
-pub fn app_loop<DISP>(
-    display: &mut DISP,
+pub fn app_loop<DI, M, RST>(
+    display: &mut mipidsi::Display<DI, M, RST>,
+    lcd_h_res:u16,
+    lcd_v_res:u16,
     seed_buffer: [u8; 32],
     icm: impl AccelDevice // You'll need to pass your accelerometer device here
 )
 where
-    DISP: DrawTarget<Color = Rgb565>,
+    DI: WriteOnlyDataCommand,
+    M: Model<ColorFormat = Rgb565>,
+    RST: OutputPin,
 {
     let accel_movement_controller = AccelMovementController::new(icm, 0.3);
 
@@ -29,7 +36,7 @@ where
     universe.initialize();
 
     loop {
-        let _ = display
-            .draw_iter(universe.render_frame().into_iter());
+        let pixel_iterator = universe.render_frame().get_pixel_iter();
+        let _ = display.set_pixels(0, 0, lcd_v_res-1, lcd_h_res, pixel_iterator);
     }
 }
