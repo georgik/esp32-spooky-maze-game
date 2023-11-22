3afb72130e60d43fab73b9282ed3821a10ee030a
Juraj Michálek <juraj.michalek@espressif.com>
2023-11-22 15:52:28 +0100

    wip: add mpu6886 wrapper

--- spooky-embedded/src/controllers/accel.rs
+++ spooky-embedded/src/controllers/accel.rs
@@ -51,3 +51,54 @@ where
         }
     }
 }
+
+#[cfg(feature = "mpu6886")]
+use embedded_hal::blocking::i2c::{Write, WriteRead};
+
+#[cfg(feature = "mpu6886")]
+use mpu6886::{Mpu6886, Mpu6886Error};
+
+// Wrapper for Mpu6886
+#[cfg(feature = "mpu6886")]
+pub struct Mpu6886Wrapper<I>(Mpu6886<I>);
+
+#[cfg(feature = "mpu6886")]
+#[derive(Debug, Clone, Copy)]
+pub struct AccelNorm {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+// Existing impl block for AccelDevice
+#[cfg(feature = "mpu6886")]
+impl<I, E> Accelerometer for Mpu6886Wrapper<I>
+where
+    I: WriteRead<Error = E> + Write<Error = E>,
+{
+    type Error = Mpu6886Error<E>;
+
+    fn accel_norm(&mut self) -> Result<AccelNorm, Self::Error> {
+        let measurement = self.0.get_acc()?;
+        Ok(AccelNorm {
+            x: measurement.x as f32,
+            y: measurement.y as f32,
+            z: measurement.z as f32,
+        })
+    }
+
+    fn sample_rate(&mut self) -> Result<f32, icm42670::accelerometer::Error<Self::Error>> {
+        todo!()
+    }
+}
+
+// Separate impl block for initialization
+#[cfg(feature = "mpu6886")]
+impl<I, E> Mpu6886Wrapper<I>
+where
+    I: WriteRead<Error = E> + Write<Error = E>,
+{
+    pub fn new(inner: Mpu6886<I>) -> Self {
+        Self(inner)
+    }
+}
