b4c85e07585847bb9129437c279ec2d5be8d941c
Juraj Michálek <juraj.michalek@espressif.com>
2023-12-06 09:35:43 +0100

    switch to axp192 from rcloran

--- m5stack-core2/src/main.rs
+++ m5stack-core2/src/main.rs
@@ -55,14 +55,69 @@ use mpu6886::Mpu6886;
 use spooky_core::engine::Engine;
 use shared_bus::BusManagerSimple;
 
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::{
+    digital::v2::OutputPin,
+    blocking::i2c::{ Read, Write, WriteRead }
+};
 
-use axp192::{ I2CPowerManagementInterface, Axp192 };
+use axp192::{ Axp192 };
 
 pub struct Universe<D> {
     pub engine: Engine<D>,
 }
 
+// Source: https://github.com/rcloran/axp192-rs/blob/main/examples/m5stack-core2.rs
+fn m5sc2_init<I2C, E>(axp: &mut axp192::Axp192<I2C>, delay: &mut Delay) -> Result<(), E>
+where
+    I2C: Read<Error = E>
+        + Write<Error = E>
+        + WriteRead<Error = E>,
+{
+    // Default setup for M5Stack Core 2
+    axp.set_dcdc1_voltage(3350)?; // Voltage to provide to the microcontroller (this one!)
+
+    axp.set_ldo2_voltage(3300)?; // Peripherals (LCD, ...)
+    axp.set_ldo2_on(true)?;
+
+    axp.set_ldo3_voltage(2000)?; // Vibration motor
+    axp.set_ldo3_on(false)?;
+
+    axp.set_dcdc3_voltage(2800)?; // LCD backlight
+    axp.set_dcdc3_on(true)?;
+
+    axp.set_gpio1_mode(axp192::GpioMode12::NmosOpenDrainOutput)?; // Power LED
+    axp.set_gpio1_output(false)?; // In open drain modes, state is opposite to what you might
+                                  // expect
+
+    axp.set_gpio2_mode(axp192::GpioMode12::NmosOpenDrainOutput)?; // Speaker
+    axp.set_gpio2_output(true)?;
+
+    axp.set_key_mode(
+        // Configure how the power button press will work
+        axp192::ShutdownDuration::Sd4s,
+        axp192::PowerOkDelay::Delay64ms,
+        true,
+        axp192::LongPress::Lp1000ms,
+        axp192::BootTime::Boot512ms,
+    )?;
+
+    axp.set_gpio4_mode(axp192::GpioMode34::NmosOpenDrainOutput)?; // LCD reset control
+
+    axp.set_battery_voltage_adc_enable(true)?;
+    axp.set_battery_current_adc_enable(true)?;
+    axp.set_acin_current_adc_enable(true)?;
+    axp.set_acin_voltage_adc_enable(true)?;
+
+    // Actually reset the LCD
+    axp.set_gpio4_output(false)?;
+    axp.set_ldo3_on(true)?; // Buzz the vibration motor while intializing ¯\_(ツ)_/¯
+    delay.delay_ms(100u32);
+    axp.set_gpio4_output(true)?;
+    axp.set_ldo3_on(false)?;
+    delay.delay_ms(100u32);
+
+    Ok(())
+}
 
 #[entry]
 fn main() -> ! {
@@ -93,11 +148,10 @@ fn main() -> ! {
 
     let bus = BusManagerSimple::new(i2c_bus);
 
-    let axp_interface = I2CPowerManagementInterface::new(bus.acquire_i2c());
-    let mut axp = Axp192::new(axp_interface);
-    axp.init().unwrap();
+    let mut axp = axp192::Axp192::new(bus.acquire_i2c());
 
     // M5Stack CORE 2 - https://docs.m5stack.com/en/core/core2
+    m5sc2_init(&mut axp, &mut delay).unwrap();
 
     let lcd_sclk = io.pins.gpio18;
     let lcd_mosi = io.pins.gpio23;
