87276c69c48d297527b2fa017cbee59a2081e34f
Juraj Michalek <juraj.michalek@gmail.com>
2024-01-23 08:23:05 +0100

    esp32-c6: update to embedded hal 1.0.0

--- spooky-embedded/src/lib.rs
+++ spooky-embedded/src/lib.rs
@@ -2,11 +2,13 @@
 pub mod app;
 pub mod button_keyboard;
 pub mod controllers {
+    #[cfg(any(feature = "icm42670", feature = "mpu6886"))]
     pub mod accel;
     pub mod button;
     pub mod composites {
         #[cfg(any(feature = "esp32s2", feature = "esp32c6"))]
         pub mod ladder_composite;
+        #[cfg(any(feature = "icm42670", feature = "mpu6886"))]
         pub mod accel_composite;
     }
     pub mod embedded;
