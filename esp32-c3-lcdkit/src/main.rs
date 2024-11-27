05ffa2c99b0cc42a587d0bb6435bc5be5fcae528
Travis Geis <ottobonn28@gmail.com>
2024-11-27 09:15:25 -0800

    Fix build on ESP32C3 LCD Kit by upgrading HAL (#74)
    * Fix build on esp32c3 LCD Kit by upgrading HAL
    ---------
    
    Co-authored-by: Juraj Michálek <juraj.michalek@espressif.com>

--- esp32-c3-lcdkit/src/main.rs
+++ esp32-c3-lcdkit/src/main.rs
@@ -18,29 +18,22 @@ use esp_println::println;
 
 use hal::{
     clock::{ClockControl, CpuClock},
-    gpio::{Input, PullUp},
+    dma::Dma,
     dma::DmaPriority,
-    gdma::Gdma,
+    dma_buffers,
+    gpio::{Input, PullUp},
     interrupt,
     peripherals::{self, Peripherals, TIMG0, TIMG1},
     prelude::*,
     riscv,
-    spi::{
-        master::{prelude::*, Spi},
-        SpiMode,
-    },
+    spi::{master::prelude::_esp_hal_spi_master_dma_WithDmaSpi2, master::Spi, SpiMode},
     timer::{Timer, Timer0, TimerGroup},
-    Delay,
-    Rng,
-    IO,
+    Delay, Rng, IO,
 };
 
-use spooky_embedded::{
-    embedded_display::{LCD_H_RES, LCD_V_RES, LCD_MEMORY_SIZE, LCD_PIXELS},
-    controllers::{accel::AccelMovementController, composites::accel_composite::AccelCompositeController}
-};
+use spooky_embedded::embedded_display::{LCD_H_RES, LCD_MEMORY_SIZE, LCD_PIXELS, LCD_V_RES};
 
-mod app;
+// mod app;
 
 mod lcdkit_composite_controller;
 mod rotary_movement_controller;
@@ -140,56 +133,33 @@ fn main() -> ! {
     let lcd_cs = io.pins.gpio7;
     let lcd_dc = io.pins.gpio2.into_push_pull_output();
     let mut lcd_backlight = io.pins.gpio5.into_push_pull_output();
-    let lcd_reset = io.pins.gpio8.into_push_pull_output();
 
     let rotary_dt = io.pins.gpio10.into_pull_up_input();
     let rotary_clk = io.pins.gpio6.into_pull_up_input();
     let rotary_switch = io.pins.gpio9.into_pull_up_input();
 
-    let dma = Gdma::new(peripherals.DMA);
+    let dma = Dma::new(peripherals.DMA);
     let dma_channel = dma.channel0;
+    let (_, mut descriptors, _, mut rx_descriptors) = dma_buffers!(4096);
 
-    let mut descriptors = [0u32; 8 * 3];
-    let mut rx_descriptors = [0u32; 8 * 3];
-
-    let spi = Spi::new(
-        peripherals.SPI2,
-        40u32.MHz(),
-        SpiMode::Mode0,
-        &clocks,
-    ).with_pins(
-        Some(lcd_sclk),
-        Some(lcd_mosi),
-        Some(lcd_miso),
-        Some(lcd_cs),
-    ).with_dma(dma_channel.configure(
-        false,
-        &mut descriptors,
-        &mut rx_descriptors,
-        DmaPriority::Priority0,
-    ));
+    let spi = Spi::new(peripherals.SPI2, 40u32.MHz(), SpiMode::Mode0, &clocks)
+        .with_pins(Some(lcd_sclk), Some(lcd_mosi), Some(lcd_miso), Some(lcd_cs))
+        .with_dma(dma_channel.configure(
+            false,
+            &mut descriptors,
+            &mut rx_descriptors,
+            DmaPriority::Priority0,
+        ));
 
     println!("SPI ready");
 
     let di = spi_dma_displayinterface::new_no_cs(LCD_MEMORY_SIZE, spi, lcd_dc);
 
-    // ESP32-S3-BOX display initialization workaround: Wait for the display to power up.
-    // If delay is 250ms, picture will be fuzzy.
-    // If there is no delay, display is blank
-    delay.delay_ms(500u32);
-    let mut display = match mipidsi::Builder::gc9a01(di)
-        // let mut display = match mipidsi::Builder::ili9341_rgb565(di)
-        .with_display_size(240 as u16, 240 as u16)
-        .with_orientation(mipidsi::Orientation::Portrait(false))
-        .with_color_order(mipidsi::ColorOrder::Bgr)
-        .with_invert_colors(mipidsi::ColorInversion::Inverted)
-        .init(&mut delay, Some(lcd_reset))
-    {
-        Ok(disp) => disp,
-        Err(_) => {
-            panic!()
-        }
-    };
+    let mut display = mipidsi::Builder::new(mipidsi::models::GC9A01, di)
+        .color_order(mipidsi::options::ColorOrder::Bgr)
+        .invert_colors(mipidsi::options::ColorInversion::Inverted)
+        .init(&mut delay)
+        .unwrap();
 
     let _ = lcd_backlight.set_high();
 
@@ -206,8 +176,7 @@ fn main() -> ! {
     let mut seed_buffer = [0u8; 32];
     rng.read(&mut seed_buffer).unwrap();
 
-    let rotary_encoder =
-        RotaryEncoder::new(rotary_dt, rotary_clk).into_standard_mode();
+    let rotary_encoder = RotaryEncoder::new(rotary_dt, rotary_clk).into_standard_mode();
 
     interrupt::enable(
         peripherals::Interrupt::TG0_T0_LEVEL,
@@ -241,23 +210,34 @@ fn main() -> ! {
     // app_loop( &mut display, seed_buffer, icm);
     println!("Starting application loop");
 
-    let demo_movement_controller = spooky_core::demo_movement_controller::DemoMovementController::new(seed_buffer);
-    let rotary_movement_controller = crate::rotary_movement_controller::RotaryMovementController::new();
-    let movement_controller = crate::lcdkit_composite_controller::LcdKitCompositeController::new(demo_movement_controller, rotary_movement_controller);
+    let demo_movement_controller =
+        spooky_core::demo_movement_controller::DemoMovementController::new(seed_buffer);
+    let rotary_movement_controller =
+        crate::rotary_movement_controller::RotaryMovementController::new();
+    let movement_controller = crate::lcdkit_composite_controller::LcdKitCompositeController::new(
+        demo_movement_controller,
+        rotary_movement_controller,
+    );
 
     use embedded_graphics::pixelcolor::Rgb565;
-    use spooky_core::{engine::Engine, spritebuf::SpriteBuf, universe::Universe};
-    use embedded_graphics_framebuf::FrameBuf;
     use embedded_graphics::prelude::RgbColor;
+    use embedded_graphics_framebuf::FrameBuf;
+    use spooky_core::{engine::Engine, spritebuf::SpriteBuf, universe::Universe};
     let mut data = [Rgb565::BLACK; LCD_PIXELS];
     let fbuf = FrameBuf::new(&mut data, LCD_H_RES as usize, LCD_V_RES as usize);
     let spritebuf = SpriteBuf::new(fbuf);
 
     let engine = Engine::new(spritebuf, Some(seed_buffer));
 
+    println!("Engine initialized");
+
     let mut universe = Universe::new_with_movement_controller(engine, movement_controller);
 
+    println!("Universe created");
+
     universe.initialize();
+
+    println!("Universe initialized");
 
     let mut clockwise_action = spooky_core::engine::Action::Right;
     let mut counter_clockwise_action = spooky_core::engine::Action::Left;
@@ -283,9 +263,8 @@ fn main() -> ! {
             Direction::None
         });
 
-
         // Switch direction of actions
-        if rotary_switch.is_low().unwrap_or(false) && !switch_in_progress  {
+        if rotary_switch.is_low().unwrap_or(false) && !switch_in_progress {
             println!("Switching direction");
             if clockwise_action == spooky_core::engine::Action::Right {
                 clockwise_action = spooky_core::engine::Action::Down;
@@ -318,9 +297,8 @@ fn main() -> ! {
 
         let pixel_iterator = universe.render_frame().get_pixel_iter();
         // -1 for some reason is necessary otherwise the display is skewed
-        let _ = display.set_pixels(0, 0, LCD_V_RES-1, LCD_H_RES, pixel_iterator);
+        let _ = display.set_pixels(0, 0, LCD_V_RES - 1, LCD_H_RES, pixel_iterator);
 
         increment_frame_counter();
-
     }
 }
