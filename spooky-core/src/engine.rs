526f9f778326c2573e74d1e8000b00d6bea6f803
Juraj Michalek <juraj.michalek@gmail.com>
2024-11-29 14:00:45 +0100

    core: update implementation

--- spooky-core/src/engine.rs
+++ spooky-core/src/engine.rs
@@ -7,8 +7,9 @@ use embedded_graphics::{
     Drawable,
 };
 
+use core::fmt::Write;
+
 use crate::{assets::Assets, maze::Maze};
-use heapless::String;
 use tinybmp::Bmp;
 
 pub enum GameState {
@@ -325,13 +326,16 @@ impl<D: embedded_graphics::draw_target::DrawTarget<Color = Rgb565>> Engine<D> {
     }
 
     fn draw_status_number(&mut self, value: u32, x: i32, y: i32) {
-        let value_message: String<5> = String::from(value);
+        let mut value_message: heapless::String<5> = heapless::String::new();
+        // Convert the u32 to a string and store it in `value_message`.
+        let _ = write!(value_message, "{}", value);
+
         let _ = Text::new(
             &value_message,
             Point::new(x, y),
             MonoTextStyle::new(&FONT_8X13, Rgb565::WHITE),
         )
-        .draw(&mut self.display);
+            .draw(&mut self.display);
     }
 
     pub fn draw_main_scene(&mut self) -> &mut D {
