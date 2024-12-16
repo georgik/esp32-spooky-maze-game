ae7bdc272d676a4bce6697891853424faf60906c
Juraj Michalek <juraj.michalek@espressif.com>
2024-12-16 18:41:11 +0100

    add ESP32-S3-BOX-3

--- esp32-s3-box/src/main.rs
+++ esp32-s3-box/src/main.rs
@@ -2,8 +2,9 @@
 #![no_main]
 
 use esp_display_interface_spi_dma::display_interface_spi_dma;
-use esp_bsp::{BoardType, DisplayConfig};
-use esp_bsp::boards::esp32s3box::{lcd_spi, lcd_display_interface, lcd_reset_pin, lcd_backlight_init};
+use esp_bsp::boards::esp32s3box::{lcd_dma_spi, lcd_display_interface, lcd_reset_pin, lcd_backlight_init,
+    i2c_init, lcd_display
+};
 
 #[allow(unused_imports)]
 use esp_backtrace as _;
@@ -25,7 +26,6 @@ use esp_hal::{
     dma::DmaPriority,
     gpio::{Level, Output},
     i2c::master::I2c,
-    peripherals::Peripherals,
     prelude::*,
     spi::master::Spi,
 };
@@ -50,19 +50,11 @@ fn main() -> ! {
 
     println!("Initializing SPI LCD driver for ESP32S3Box");
 
-    // Initialize I2C for accelerometer
-    let i2c_sda = peripherals.GPIO8;
-    let i2c_scl = peripherals.GPIO18;
-    let i2c = I2c::new(peripherals.I2C0, esp_hal::i2c::master::Config::default())
-        .with_sda(i2c_sda)
-        .with_scl(i2c_scl);
-
-    // Initialize DMA for SPI
-    let dma = Dma::new(peripherals.DMA);
-    let dma_channel = dma.channel0;
+    // Use the `lcd_i2c_init` macro to initialize I2C for accelerometer
+    let i2c = i2c_init!(peripherals);
 
     // Use the `lcd_spi` macro to initialize the SPI interface
-    let spi = lcd_spi!(peripherals, dma_channel);
+    let spi = lcd_dma_spi!(peripherals);
 
     println!("SPI ready");
 
@@ -72,19 +64,7 @@ fn main() -> ! {
     // ESP32-S3-BOX display initialization workaround: Wait for the display to power up.
     delay.delay_ns(500_000u32);
 
-    // Use the `lcd_reset_pin` macro to set the reset pin
-    let lcd_reset = lcd_reset_pin!(peripherals);
-
-    // Initialize the display using the builder pattern
-    let mut display = mipidsi::Builder::new(mipidsi::models::ILI9341Rgb565, di)
-        .display_size(240, 320)
-        .orientation(
-            mipidsi::options::Orientation::new()
-                .flip_vertical()
-                .flip_horizontal(),
-        )
-        .color_order(mipidsi::options::ColorOrder::Bgr)
-        .reset_pin(lcd_reset)
+    let mut display = lcd_display!(peripherals, di, orientation)
         .init(&mut delay)
         .unwrap();
 
